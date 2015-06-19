3fa5978d989d7d1d15e7bd4ab9e952a4b9e87eb1
amy6春晓 <183993359@qq.com>
2015-06-19 21:12:42 +0800

    binary tree added

---
+++ src/com/cqupt/dataStructure/model/BinaryTreeNode.java
@@ -0,0 +1,40 @@
+package com.cqupt.dataStructure.model;
+
+/**
+ * 
+ * Title: BinaryTreeNode.java
+ * 
+ * @description:二叉树
+ * @author liucx
+ * @created 2015年6月19日 下午9:10:17
+ */
+public class BinaryTreeNode<T> {
+	T data;
+	BinaryTreeNode<T> leftChild;
+	BinaryTreeNode<T> rightChild;
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public BinaryTreeNode<T> getLeftChild() {
+		return leftChild;
+	}
+
+	public void setLeftChild(BinaryTreeNode<T> leftChild) {
+		this.leftChild = leftChild;
+	}
+
+	public BinaryTreeNode<T> getRightChild() {
+		return rightChild;
+	}
+
+	public void setRightChild(BinaryTreeNode<T> rightChild) {
+		this.rightChild = rightChild;
+	}
+
+}
