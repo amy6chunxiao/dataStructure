951d379ad26e1b0193a2ebf218a28a0bfc89dc22
amy6春晓 <183993359@qq.com>
2015-06-25 16:53:58 +0800

    bug check

--- test/algorithm/sortTest/HeapSortTest.java
+++ test/algorithm/sortTest/HeapSortTest.java
@@ -7,7 +7,7 @@ public class HeapSortTest {
 
 	public static void main(String[] args) {
 		Sort<Double> heapSort = new HeapSort<Double>();
-		Double[] r = {2.0, 9.0, 5.0, 3.0, 21.0, 4.0, 6.0};
+		Double[] r = { 2.0, 9.0, 5.0, 3.0, 21.0, 4.0, 6.0 };
 		heapSort.sort(r, r.length);
 
 		System.out.println("排序完成");
